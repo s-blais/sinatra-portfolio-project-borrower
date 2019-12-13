355c8afb4c2c135dfd7e196f975aaec78a37b972
s-blais <stephen@s-blais.com>
2019-12-13 14:17:26 -0500

    Enable sessions in app and set ENV in environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-#   configure do
-#     set :public_folder, 'public'
-#     set :views, 'app/views'
-#     enable :sessions
-#     set :session_secret, "password_security"
-#   end
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
 
 #   get '/' do
 #     erb :index
